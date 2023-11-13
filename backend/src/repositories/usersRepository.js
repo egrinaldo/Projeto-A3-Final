bdd8c7bf4338cae7bd70bdab0d1cdee1ae2a46aa
Gabriel Cedraz <cedrazdev@gmail.com>
2023-11-13 19:28:10 -0300

    feat: cria consulta que cria usuario

--- backend/src/repositories/usersRepository.js
+++ backend/src/repositories/usersRepository.js
@@ -11,21 +11,29 @@ async function getAllUsers() {
 
 // Cria um novo usuário
 async function createUser(userData) {
-  console.log("bateu")
-   return await prisma.user.create({
-    data: {
-      name: userData.name,
-      email: userData.email,
-      password: userData.password,
-      username: userData.username
-    },
-  },
-); 
+
+  const query = `
+    INSERT INTO user (name, email, password, username)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  const params = [userData.name, userData.email, userData.password, userData.username];
+
+  const userCreated =  await prisma.$executeRaw`INSERT INTO User (name, email, password, username)
+   VALUES (${userData.name}, ${userData.email}, ${userData.password}, ${userData.username});`
+
+   if (userCreated){
+    return getUserByEmail(userData.email)
+   }
 }
 
 // Obtém um usuário pelo ID
 async function getUserById(userId) {
   return await prisma.$queryRaw`SELECT * FROM User WHERE id = ${userId}`
+}
+
+async function getUserByEmail(email){
+  return await prisma.$queryRaw`SELECT * FROM User WHERE email = ${email}`	
 }
 
 // Atualiza um usuário pelo ID
