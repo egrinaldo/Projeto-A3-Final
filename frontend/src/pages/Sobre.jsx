a20ecf76e4835f49fbdc66f95073b9ac3f723c39
Egrinaldo Junior <egrinaldo19@gmail.com>
2023-11-14 18:57:59 -0300

    .

--- frontend/src/pages/Sobre.jsx
+++ frontend/src/pages/Sobre.jsx
@@ -1,3 +1,4 @@
+import SobrePag from "../Sobre/SobrePag";
 import Navbar from "../barra_navegacao/barnav";
 
 
@@ -5,6 +6,7 @@ export default function Sobre() {
   return (
     <div>
       <Navbar />
+      <SobrePag />
     </div>
   )
 }
