58cc8d43b451a91cb765fc858e9a0715ebbddd76
Gabriel Cedraz <cedrazdev@gmail.com>
2023-11-20 15:07:05 -0300

    ajustes menu dependendo do usuario logado ou nao

--- frontend/src/barra_navegacao/acesso_user/User_Ac.jsx
+++ frontend/src/barra_navegacao/acesso_user/User_Ac.jsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom'
-import './User_Ac.css'
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../app/hooks/useAuth';
+import './User_Ac.css';
 export default function UserAcess() {
+    const { signedIn, signout } = useAuth();
     return (
         <div className='UserA'>
             <a href="/Login">
                 <img src="src/assets/img_barnav/controle-de-video-game.png" alt="Logo Do Usuario" />
             </a>
-            <p><Link to="/Login">Entrar</Link></p>
+            {signedIn && <button onClick={signout}>Sair</button>}
+            {!signedIn && <Link to="/Login">Entrar</Link>}
         </div>
     )
 }
--- frontend/src/barra_navegacao/menu/menu_nav.jsx
+++ frontend/src/barra_navegacao/menu/menu_nav.jsx
@@ -1,11 +1,15 @@
-import './menu_nav.css'
+import { useAuth } from '../../app/hooks/useAuth';
+import './menu_nav.css';
 
 export default function NavMenu() {
+    const { signedIn } = useAuth();
     return (
         <div className="menu_nave">
             <ul>
                 <li><a href="/">HOME</a></li>
                 <li><a href="/Sobre">SOBRE</a></li>
+                {signedIn && <li><a href="/MinhaArea">MINHA ÁREA</a></li>}
+                {signedIn && <li><a href="/PlataformaCrud">PLATAFORMA</a></li>}
             </ul>
         </div>
     )
